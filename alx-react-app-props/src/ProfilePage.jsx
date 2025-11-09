535da06d8378bf34c0b920e45a30428f7d0a0ea2
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-09 13:32:35 +0100

    Add files via upload

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default ProfilePage;
