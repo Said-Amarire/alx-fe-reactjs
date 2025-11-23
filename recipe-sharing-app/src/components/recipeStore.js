b9bda654123c677f3fdd8a993359527b386175f9
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-23 16:09:25 +0100

    Create recipeStore.js

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,59 @@
+import create from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  favorites: [],
+  recommendations: [],
+  searchTerm: '',
+  filteredRecipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+      filteredRecipes: [...state.filteredRecipes, newRecipe],
+    })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+      filteredRecipes: state.filteredRecipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+      filteredRecipes: state.filteredRecipes.filter(
+        (recipe) => recipe.id !== recipeId
+      ),
+      favorites: state.favorites.filter((id) => id !== recipeId),
+    })),
+
+  setSearchTerm: (term) =>
+    set((state) => ({
+      searchTerm: term,
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      ),
+    })),
+
+  addFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: [...new Set([...state.favorites, recipeId])],
+    })),
+
+  removeFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: state.favorites.filter((id) => id !== recipeId),
+    })),
+
+  generateRecommendations: () =>
+    set((state) => ({
+      recommendations: state.recipes.filter(
+        (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
+      ),
+    })),
+}));
