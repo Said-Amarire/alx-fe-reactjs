cb508e16057448434ec98845df6903c65ee86f34
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-23 15:52:43 +0100

    Add files via upload

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <hr />
+      <RecipeList />
+    </div>
+  );
+}
