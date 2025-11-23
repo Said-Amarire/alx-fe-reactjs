3eb38acb4ac1cd2e44a3b9c7dc8761a78c1157fb
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-23 16:01:49 +0100

    Update recipeStore.js

--- recipe-sharing-app/src/stores/recipeStore.js
+++ recipe-sharing-app/src/stores/recipeStore.js
@@ -2,16 +2,38 @@ import create from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+      filteredRecipes: [...state.filteredRecipes, newRecipe],
+    })),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
+      filteredRecipes: state.filteredRecipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
     })),
+
   deleteRecipe: (recipeId) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+      filteredRecipes: state.filteredRecipes.filter(
+        (recipe) => recipe.id !== recipeId
+      ),
+    })),
+
+  setSearchTerm: (term) =>
+    set((state) => ({
+      searchTerm: term,
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      ),
     })),
 }));
