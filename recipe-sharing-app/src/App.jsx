70628f8365c1b849fadd98f299e15b5724d6ca58
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-23 23:00:00 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,47 +1,37 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
-const App = () => {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </div>
-  );
-};
-
-// Wrapper to read URL param
-import { useParams } from 'react-router-dom';
 const RecipeDetailsWrapper = () => {
   const { id } = useParams();
   return <RecipeDetails recipeId={Number(id)} />;
 };
 
+const App = () => {
+  return (
+    <Router>
+      <div style={{ padding: '20px' }}>
+        <h1>Recipe Sharing App</h1>
+
+        <AddRecipeForm />
+        <SearchBar />
+
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        </Routes>
+
+        <FavoritesList />
+        <RecommendationsList />
+      </div>
+    </Router>
+  );
+};
+
 export default App;
-
-
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
-
-<div style={{ padding: '20px' }}>
-  <h1>Recipe Sharing App</h1>
-  <AddRecipeForm />
-  <SearchBar />
-  <Routes>
-    <Route path="/" element={<RecipeList />} />
-    <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-  </Routes>
-
-  <FavoritesList />
-  <RecommendationsList />
-</div>
-
