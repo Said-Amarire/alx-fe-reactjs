f379b2ae661b9de9214f75366e61bfe7c446fe46
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-23 23:50:01 +0100

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,1 +1,13 @@
-<h1 className="text-blue-500 text-4xl font-bold">Tailwind Works!</h1>
+import React from 'react';
+import Search from './components/Search';
+
+const App = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
