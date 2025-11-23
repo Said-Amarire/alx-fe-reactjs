0d97df7a9dabe2bf638f41188730d4e73a18f04b
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-23 15:52:23 +0100

    Delete recipe-sharing-app directory

--- recipe-sharing-app/src/stores/recipeStore.js
+++
@@ -1,7 +0,0 @@
-// src/stores/recipeStore.js
-import create from 'zustand';
-
-export const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-}));
