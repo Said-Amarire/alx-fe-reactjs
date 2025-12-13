d5a16361ef84be0d4cef0b35efeaa6d59f63b53c
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-13 20:26:59 +0100

    Delete react-todo (test) directory

--- react-todo (test)/src/components/PostsComponent.jsx
+++
@@ -1,7 +0,0 @@
-// PostsComponent.jsx
-const { isError, isLoading, data } = useQuery('posts', fetchPosts, {
-    cacheTime: 5000,
-    staleTime: 10000,
-    refetchOnWindowFocus: true,
-    keepPreviousData: true
-})
