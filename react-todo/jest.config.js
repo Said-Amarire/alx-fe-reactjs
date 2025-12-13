95b403c03f8bed5a82605450f57828722c6490a7
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-13 20:27:19 +0100

    Delete react-todo directory

--- react-todo/jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.cjs
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  transform: {
-    "^.+\\.[tj]sx?$": "babel-jest"
-  }
-};
