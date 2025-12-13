d9adeee5e8014a5f3bac5f2941727bfe52df37d5
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-13 20:51:02 +0100

    up

--- react-todo/jest.config.cjs
+++ react-todo/jest.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
   transform: {
     "^.+\\.jsx?$": "babel-jest"
   },
+  moduleFileExtensions: ["js", "jsx", "json", "node"]
 };
